484d1c8d43de9b7a7f5c7bf492de7eb763a67ef5
paras01-patel <paraschandrawanshi649@gmail.com>
2026-05-26 11:52:14 +0530

    login /sign

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def sign(req):
+    if 
+    return render(req,'sign')
+
+def login(req):
+    return render(req,"login.html")
