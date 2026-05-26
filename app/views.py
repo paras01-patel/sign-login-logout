f30e3a5e39f765b471eb4c2abb740ebbf3a7a36e
paras01-patel <paraschandrawanshi649@gmail.com>
2026-05-26 12:12:07 +0530

    sign/logic

--- app/views.py
+++ app/views.py
@@ -1,11 +1,59 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+
+# SIGNUP
 
 def sign(req):
-    if 
-    return render(req,'sign')
+
+    if req.method == "POST":
+
+        username = req.POST.get('username')
+        email = req.POST.get('email')
+        password1 = req.POST.get('password1')
+        password2 = req.POST.get('password2')
+
+        # PASSWORD CHECK
+
+        if password1 != password2:
+
+            messages.error(req, "Password does not match")
+            return redirect('sign')
+
+        # USERNAME CHECK
+
+        if User.objects.filter(username=username).exists():
+
+            messages.error(req, "Username already exists")
+            return redirect('sign')
+
+        # EMAIL CHECK
+
+        if User.objects.filter(email=email).exists():
+
+            messages.error(req, "Email already exists")
+            return redirect('sign')
+
+        # CREATE USER
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password1
+        )
+
+        user.save()
+
+        messages.success(req, "Signup Successfully")
+
+        return redirect('login')
+
+    return render(req, 'sign.html')
+
+
+# LOGIN PAGE
 
 def login(req):
-    return render(req,"login.html")
+    if req.method=="POST":
+    return render(req, "login.html")
